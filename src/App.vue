ba8d7006b4290919ec4054b6b9843d1934be2eec
loyet <thomas.loye@etu.umontpellier.fr>
2025-11-24 09:35:28 +0100

    feat : init frontend accueil

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,28 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {RouterLink, RouterView} from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <header class="general-header">
+    <a class="header-logo" href="/">
+      <img src="@/assets/img/logos/black.png" alt="">
+    </a>
+    <div class="header-links">
+      <RouterLink to="/" class="header-link">
+        Accueil
+      </RouterLink>
+      <span class="header-link">Fonctionnalités</span>
+      <span class="header-link">FAQ</span>
+      <span class="header-link">Connexion</span>
     </div>
+    <a href="" class="header-button">
+      Inscription
+    </a>
   </header>
 
-  <RouterView />
+  <RouterView class="router-view"/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style>
+@import "@/assets/styles/generalView.css";
 </style>
